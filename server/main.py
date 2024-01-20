c34b9695de8e7f9d6edb5524b45eee5e197a90a1
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2024-01-20 15:32:02 -0800

    POC endpoints

--- server/main.py
+++ server/main.py
@@ -1,13 +1,12 @@
 # Starts API
 import base64
-
 from flask import Flask, request
 import json
 from pathlib import Path
 
 # Constants
 SERVER_PATH = Path(__file__).parent
-BOARD_PATH = SERVER_PATH / 'data' / 'board.png'
+BOARD_PATH = SERVER_PATH / 'data/board.png'
 
 # Init flask
 app = Flask(__name__)
@@ -17,17 +16,37 @@ API_PORT = 2620
 # Endpoints
 @app.route('/addImage', methods=['POST'])
 def add_image():
-    pass
+    # TODO proof of concept, print b64
+    try:
+        img_b64 = request.json.get("image")
+        print('Image recieved: ' + img_b64)
+        return json.dumps({
+            "status": "success",
+            "data": None
+        })
+    except Exception as e:
+        return json.dumps({
+            "status": "error",
+            "data": None,
+            "message": "Internal Server Error: " + repr(e)
+        })
 
 
 @app.route('/getBoard')
 def get_board():
     # TODO proof of concept, serve static board image
-    with BOARD_PATH.open() as f:
-        board_b64 = base64.b64encode(f.read())
+    try:
+        with BOARD_PATH.open('rb') as f:
+            board_b64 = base64.b64encode(f.read())
+            return json.dumps({
+                "status": "success",
+                "data": {"board": str(board_b64)}
+            })
+    except Exception as e:
         return json.dumps({
-            "status": "success",
-            "data": {"board": board_b64}
+            "status": "error",
+            "data": None,
+            "message": "Internal Server Error: " + repr(e)
         })
 
 
