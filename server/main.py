f25ec8f0194b63b1f3fe42b78b3e8a168bd8e2d7
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2024-01-20 14:37:06 -0800

    test getBoard

--- server/main.py
+++ server/main.py
@@ -1,10 +1,15 @@
 # Starts API
-from flask import Flask
+import base64
 
-# Server constants
+from flask import Flask, request
+import json
+from pathlib import Path
 
+# Constants
+SERVER_PATH = Path(__file__).parent
+BOARD_PATH = SERVER_PATH / 'data' / 'board.png'
 
-# Init flask server
+# Init flask
 app = Flask(__name__)
 API_PORT = 2620
 
@@ -17,4 +22,14 @@ def add_image():
 
 @app.route('/getBoard')
 def get_board():
-    pass
+    # TODO proof of concept, serve static board image
+    with BOARD_PATH.open() as f:
+        board_b64 = base64.b64encode(f.read())
+        return json.dumps({
+            "status": "success",
+            "data": {"board": board_b64}
+        })
+
+
+# Start flask API
+app.run(port=API_PORT, host="0.0.0.0")
