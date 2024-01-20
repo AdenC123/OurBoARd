5ca8e68a43dd0674048643cd74e1fe1278790fd1
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2024-01-20 14:34:23 -0800

    addImage, getBoard requests (base64, dk if async works)

--- frontend/src/fetch/addImage.js
+++ frontend/src/fetch/addImage.js
@@ -2,7 +2,15 @@ import {addImageSlug, serverLink} from "./utils";
 
 // Takes image and uploads it to the Python backend
 // * img = image file
-function addImage(img) {
+async function addImage(img) {
+    let str;
+    imgToBase64(img)
+        .then(base64String => {
+            str = base64String;
+        })
+        .catch(error => {
+            console.error(error);
+        });
     fetch(serverLink + addImageSlug, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
@@ -13,10 +21,28 @@ function addImage(img) {
         },
         redirect: 'follow', // manual, *follow, error
         referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify({image: img}) // body data type must match "Content-Type" header
+        body: JSON.stringify({image: str}) // body data type must match "Content-Type" header
     })
         .then(response => {})
         .catch(error => console.log("Error: " + error))
 }
+
+function imgToBase64(blob) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+
+        reader.onload = () => {
+            const base64String = reader.result.split(',')[1];
+            resolve(base64String);
+        };
+
+        reader.onerror = (error) => {
+            reject(error);
+        };
+
+        reader.readAsDataURL(blob);
+    });
+}
+
 
 export default addImage;
--- frontend/src/fetch/getBoard.js
+++ frontend/src/fetch/getBoard.js
@@ -1,28 +1,33 @@
-import {addImageSlug, getBoardSlug, serverLink} from "./utils";
+import { addImageSlug, getBoardSlug, serverLink } from "./utils";
 
 // Gets the current board from backend
 // Returns image file
-function getBoard() {
-    fetch(serverLink + getBoardSlug, {
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: "" // body data type must match "Content-Type" header
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => data.image)
-        .catch(error => console.log("Error: " + error))
+async function getBoard() {
+    try {
+        const response = await fetch(serverLink + getBoardSlug, {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: ''
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        let image = new Image();
+        image.src = 'data:image/png;base64,' + data.image;
+        return image;
+    } catch (error) {
+        console.log('Error:', error);
+    }
 }
 
 export default getBoard;
