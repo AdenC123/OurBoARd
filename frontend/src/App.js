bf37d3a98bc11441b5bad5399f448eb9b6894252
Andrew Ahn <andrewahn21@gmail.com>
2024-01-20 14:20:36 -0800

    The HTML file containiner AR.js code.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js" />
       <h1>ourboARd</h1>
     </div>
   );
