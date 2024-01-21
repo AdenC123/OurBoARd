fcd2e3e0c0c87dd474d1359262524d27df9eb7a1
zayan-sheikh <dbzayan@gmail.com>
2024-01-20 17:32:29 -0800

    Removed unneeded code

--- frontend/src/components/Uploader.js
+++ frontend/src/components/Uploader.js
@@ -42,15 +42,4 @@ export default function Uploader() {
             </form>
         </main>
     )
-
-    async function getBase64(file) {
-        return new Promise((resolve, reject) => {
-          const reader = new FileReader()
-          reader.readAsDataURL(file)
-          reader.onload = () => {
-            resolve(reader.result)
-          }
-          reader.onerror = reject
-        })
-      }
 }
