e8f6fdb360bf1e308bb43ac23faf872e6a808230
zayan-sheikh <dbzayan@gmail.com>
2024-01-20 17:26:10 -0800

    Added image upload functionality to base64, does not contact server yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Uploader from './components/Uploader';
 
 function App() {
   return (
     <div className="App">
       <h1>ourboARd</h1>
+      <Uploader/>
+      
+      
+      {/* <button className="uploadFile" style={{
+        padding: 10,
+        border: 0,
+      }}>+</button> */}
+
     </div>
   );
 }
---
+++ frontend/src/components/Uploader.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import './Uploader.css'
+// import{ MdCloudUpload, MdDelete} from 'react-icons/md'
+// import { AiFillFileImage } from 'react-icons/ai'
+
+export default function Uploader() {
+
+    const [image, setImage] = useState(null)
+    const [fileName, setFileName] = useState("No selected file")
+    return (
+        <main>
+            <form
+            onClick={() => document.querySelector(".input-field").click()}>
+                <input type="file" accept='image/*' className="input-field" hidden
+                onChange={({ target: {files}}) => {
+                    files[0] && setFileName(files[0].name)
+                    if (files && files.item(0).type.match('image.*')){
+                        const fileReader = new FileReader();
+                        const string = fileReader.readAsDataURL(files[0])
+                        console.log(string)
+                        console.log(fileReader)
+                        // resolve for axios
+                        // fileReader.onload = () => {
+                        //     resolve(fileReader.result);
+                        // };
+
+                        // reader.onerror = (error) => {
+                        //     reject(error);
+                        // }
+                        setImage(URL.createObjectURL(files[0]))
+                        console.log(image)
+                    }
+                }}
+                />
+                
+                {image ?
+                    <img src={image} width={300} height={150} alt={fileName} /> 
+                    :
+                    <img src={"./media/image-upload-icon.png"} width={300} height={150} alt={"Upload Image"} /> 
+                }
+                
+            </form>
+        </main>
+    )
+
+    async function getBase64(file) {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader()
+          reader.readAsDataURL(file)
+          reader.onload = () => {
+            resolve(reader.result)
+          }
+          reader.onerror = reject
+        })
+      }
+}
