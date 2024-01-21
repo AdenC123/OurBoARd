ef355abc0be04d5da1db88e92833d7ecd4af900d
Andrew Ahn <andrewahn21@gmail.com>
2024-01-20 21:56:07 -0800

    Added an "add image" button to cameraScreen.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import Uploader from './components/Uploader';
 
 function App() {
+  var perf = require('./imageTexture.html')
+
   return (
     <div className="App">
       <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js" />
       <h1>ourboARd</h1>
       <Uploader/>
+
+      <iframe src={perf}></iframe>
       
       
       {/* <button className="uploadFile" style={{
